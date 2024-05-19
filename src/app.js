51865e149bbbdaefa087e807f11be72dc3e13fbd
rupsa45 <drupsa767@gmail.com>
2024-05-19 17:37:37 +0530

    chnaged

--- src/app.js
+++ src/app.js
@@ -8,12 +8,25 @@ const app=express();
 
 
 app.use(morgan('dev'));
+const corsOptions = {
+    origin: ['http://localhost:5173', 'https://twitter-clone-frontend-gyut.onrender.com'],
+    optionsSuccessStatus: 200,
+    credentials: true, // Include this in the options if you want to support credentials
+};
 
-app.use(cors({
-    origin:process.env.CORS_ORIGIN,
-    credentials:true
-}))
+const dynamicCorsOptions = {
+    origin: (origin, callback) => {
+        if (process.env.CORS_ORIGIN) {
+            callback(null, process.env.CORS_ORIGIN);
+        } else {
+            callback(null, corsOptions.origin);
+        }
+    },
+    optionsSuccessStatus: 200,
+    credentials: true,
+};
 
+app.use(cors(dynamicCorsOptions));
 
 app.use(express.json({limit: "5mb"}))// to parse req.body
 
